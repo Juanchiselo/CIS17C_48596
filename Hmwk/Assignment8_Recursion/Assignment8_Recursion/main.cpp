d361412f70d15bfdccea60f6a4850ef9a2f85cc3
Jose Sandoval <juanchis_sandoval@hotmail.com>
2014-11-04 23:30:57 -0800

    Assignment 8 - Recursion Finished

---
+++ Hmwk/Assignment8_Recursion/Assignment8_Recursion/main.cpp
@@ -0,0 +1,44 @@
+/*
+* Jose Sandoval
+* CIS-17C: C++ Programming
+* November 4, 2014
+* Assignment #8: Recursion
+* Description: Recursive function that predicts savings based on present
+			   value, annual interest rate in percentage and number of
+			   years.
+*/
+
+// System Libraries
+#include <iostream>
+
+using namespace std;
+
+float savings(float, float, int);
+
+int main(int argc, char** argv)
+{
+	// Declare Variables
+	float presentValue;
+	float interestRate;
+	int years;
+
+	// Ask for user input
+	cout << "What is the Present Value? " << endl;
+	cin >> presentValue;
+	cout << "What is the annual interest rate %? " << endl;
+	cin >> interestRate;
+	cout << "How many years?" << endl;
+	cin >> years;
+
+	// Print result
+	cout << "Your Future Value is " << savings(presentValue, (interestRate / 100), years) << endl;
+	return 0;
+}
+
+float savings(float presentValue, float interestRate, int years)
+{
+	if (years > 0)
+		return (savings(presentValue, interestRate, years - 1) * (1 + interestRate));
+
+	return presentValue;
+}
